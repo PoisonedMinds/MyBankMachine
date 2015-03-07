8b29fb0c65f7bf80bfe5f44f8610a2eea44adaba
PoisonedMinds <steven_biro@hotmail.com>
2015-03-07 16:19:13 -0500

    done

--- MyBankMachine/src/mybankmachine/ATM.java
+++ MyBankMachine/src/mybankmachine/ATM.java
@@ -23,27 +23,36 @@ public class ATM {
         Double intRate;
         int numDays;
         JOptionPane.showMessageDialog(null,"Hello and welcome to " + bank + " your curren"
-                + "t balance is: " + balance);
+                + "t balance is: $" + balance);
         //        test=Integer.parseInt(JOptionPane.showInputDialog("What would you like to do today?"))
         String[] buttons = {"Withdraw", "Deposit", "Calculate Dailiy Interest","Show Balance","Exit"};
 
-        int test = JOptionPane.showOptionDialog(null, "What would you like to do?", "ATM",
-                JOptionPane.PLAIN_MESSAGE,1, null, buttons, buttons[4]);
+        test = JOptionPane.showOptionDialog(null, "What would you like to do?", "ATM",
+                JOptionPane.PLAIN_MESSAGE,3, null, buttons, buttons[4]);
 
         System.out.println(test);
         while (test != 4) {
             if (test==0) {
                 money=Double.parseDouble(JOptionPane.showInputDialog("How much would you like to withdraw?",20));
                 balance=balance-money;
+                                JOptionPane.showMessageDialog(null,"Your new current balance is: $"+balance);
+
             } else if (test==1) {
                 money=Double.parseDouble(JOptionPane.showInputDialog("How much would you like to deposit?",20));
                 balance=balance+money;
+                                JOptionPane.showMessageDialog(null,"Your new current balance is: $"+balance);
+
             } else if (test==2) {
                 intRate=Double.parseDouble(JOptionPane.showInputDialog("Input your interest rate.",10));
                 numDays=Integer.parseInt(JOptionPane.showInputDialog("Input the amount of days",1));
                 addDailyInterest(intRate,numDays);
-            }
+                                JOptionPane.showMessageDialog(null,"Your new current balance is: $"+balance);
 
+            } else if (test==3) {
+                JOptionPane.showMessageDialog(null,"Your new current balance is: $"+balance);
+            }
+           test = JOptionPane.showOptionDialog(null, "Would you like to do anything else?", "ATM",
+                JOptionPane.PLAIN_MESSAGE,3, null, buttons, buttons[4]);  
         }
 System.exit(0);
     }
@@ -52,7 +61,7 @@ System.exit(0);
     if (intRate >= 0 && numDays >= 0) {
         double i = (intRate/100) / 365;
         double A = balance * Math.pow((1.0 + i), numDays);
-        System.out.println("The interest earned in " + numDays + " = " + (A - balance));
+        JOptionPane.showMessageDialog(null,"The interest earned in " + numDays + " days is: $" + (A - balance));
         balance = A;
     }
     else {
