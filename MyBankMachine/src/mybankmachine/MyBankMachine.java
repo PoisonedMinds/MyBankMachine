58b6639902bd1202fa2fd1d1c7b4deca98ca0ec2
PoisonedMinds <steven_biro@hotmail.com>
2015-03-21 14:26:47 -0400

    commenting

--- MyBankMachine/src/mybankmachine/MyBankMachine.java
+++ MyBankMachine/src/mybankmachine/MyBankMachine.java
@@ -1,22 +1,25 @@
-
-
 package mybankmachine;
+
 import javax.swing.*;
+
 /**
  * @title MyBankMachine
  * @author Steven Biro
  * @teacher Mr. J. Carron
  * @date 6-Mar-2015 11:56:19 AM
- * @purpose The purpose of this program is to
+ * @purpose The purpose of this program is to start a program that models a bank
+ * machine
  */
 public class MyBankMachine {
 
     public static void main(String[] args) {
         String bank;
         Double balance;
-    bank=JOptionPane.showInputDialog("Input your bank's name.","TD");
-    balance=Double.parseDouble(JOptionPane.showInputDialog("Input your current balance.",0));
-        ATM atm1=new ATM(bank, balance);
+        //get basic info for bank machine
+        bank = JOptionPane.showInputDialog("Input your bank's name.", "TD");
+        balance = Double.parseDouble(JOptionPane.showInputDialog("Input your current balance.", 0));
+        //start the bank machine
+        ATM atm1 = new ATM(bank, balance);
     }
 
 }
